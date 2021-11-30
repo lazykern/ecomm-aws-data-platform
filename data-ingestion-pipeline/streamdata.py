fed4eba98056367f274b15acf32744198bfcdeea
Phusit Somboonyingsuk <phusitsom@gmail.com>
2021-12-01 00:15:47 +0700

    Update simulator

--- data-ingestion-pipeline/streamdata.py
+++ data-ingestion-pipeline/streamdata.py
@@ -1,17 +1,32 @@
 import requests
 import time
+import json
+import sys
 import pandas as pd
 from dotenv import dotenv_values
 
+with open("config.json") as conf:
+    config = json.load(conf)
 
-def stream_data(data: pd.DataFrame, url: str, sleep_time: int = 0, n_rows: int = 0):
-    if n_rows: data = data.head(n_rows)
 
+def stream_data(data: pd.DataFrame, url: str, sleep_time: int = 0, n_rows: int = 10, continue_from_last: bool = True):
+    
+    if continue_from_last:
+        current_index = config["current_index"]
+        data = data.loc[current_index:]
+
+    if n_rows:
+        data = data.head(n_rows)
     for i, row in enumerate(data.to_dict(orient="records")):
         response = requests.post(url, json=row)
-        print(i, response.status_code)
+        print(i+current_index, response.status_code)
 
         time.sleep(sleep_time)
+
+    config["current_index"] += n_rows
+
+    with open("config.json", "w") as conf:
+        conf.write(json.dumps(config, indent=4))
 
 
 AWS_CONFIG = dotenv_values("aws.env")
