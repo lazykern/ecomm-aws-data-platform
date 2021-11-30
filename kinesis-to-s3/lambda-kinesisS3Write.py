2e80a1ad75c58b08fee33089dc5e25d6554271f8
Phusit Somboonyingsuk <phusitsom@gmail.com>
2021-12-01 00:16:05 +0700

    update kinesisS3Write

--- kinesis-to-s3/lambda-kinesisS3Write.py
+++ kinesis-to-s3/lambda-kinesisS3Write.py
@@ -14,12 +14,12 @@ kinesisRecords = []
 def lambda_handler(event, context):
     for record in event['Records']:
         payload = base64.b64decode(record['kinesis']['data'])
-        kinesisRecords.append(payload)
+        kinesisRecords.append(str(payload))
 
     ex_string = '\n'.join(kinesisRecords)
 
     mykey = 'output-' + timestampStr + '.txt'
 
-    response = s3_client.put_object(Body=ex_string, Bucket='aws-de-project', Key=mykey)
+    response = s3_client.put_object(Body=ex_string, Bucket='ecommerce-customer-datalake', Key=mykey)
 
-    return response.json()
+    return 'Successfully processed {} records.'.format(len(event['Records']))
